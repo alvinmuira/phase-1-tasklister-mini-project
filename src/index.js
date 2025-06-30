a4a61082b77ebb494e5abd13ccfaff53103a70b0
Alvin Muira <alvinmuira@gmail.com>
2025-06-30 21:25:24 +0300

    Finnish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector("form");
+  form.addEventListener ("submit", e => {
+    e.preventDefault();
+    runValue(e.target.new_task_description.value);
+    e.target.reset();
+  });
+  function runValue(value) {
+    const li = document.createElement(`li`);
+    const p = document.createElement(`p`);
+    const deleteButton = document.createElement(`button`);
+    p.textContent = value;
+    deleteButton.textContent = "Done";
+    li.appendChild(p);
+    li.appendChild(deleteButton);
+    const tasks = document.querySelector("#tasks").appendChild(li);
+    deleteButton.addEventListener("click", (e) => {
+      e.target.parentNode.remove();
+    });
+  }
 });
+
